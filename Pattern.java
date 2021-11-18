6ba6ed2fff20f2fc72879613b59196526afad716
Akshit Kashyap <60666183+akshitkashyap@users.noreply.github.com>
2021-11-18 13:16:54 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class Pattern {
+	public static void main(String[] args) {
+		int r=4;
+		for(int i=r-1;i>=0;i--) 
+		{
+			for(int j=0;j<=i;j++) 
+			{
+				System.out.print("*"+" ");			
+			
+		}
+		System.out.println();
+		}
+	}
+
+}
+
